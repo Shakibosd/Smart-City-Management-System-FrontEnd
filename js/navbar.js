3aca24e8da9f51faba5d4037a012055eed3474da
Syed Nazmus Shakib <122551588+Shakibosd@users.noreply.github.com>
2024-10-24 02:26:09 +0600

    signup and login added

---
+++ js/navbar.js
@@ -0,0 +1,111 @@
+// fetch("navbar.html")
+//   .then((res) => res.text())
+//   .then((data) => {
+//     document.getElementById("navbar").innerHTML = data;
+
+//     const navElement = document.getElementById("nav-element");
+//     const token = localStorage.getItem("authToken");
+
+//     if (token) {
+//       fetch("http://127.0.0.1:8000/admins/admins/", {
+//         method: "GET",
+//         headers: {
+//           "Content-Type": "application/json",
+//           Authorization: `token ${token}`,
+//         },
+//       })
+//         .then((response) => response.json())
+//         .then((result) => {
+//           if (result.is_admin) {
+//             navElement.innerHTML += `
+//                 <li class="nav-item">
+//                     <a class="nav-link text-light" href=""><b>Home</b></a>
+//                 </li>
+//                 <li class="nav-item">
+//                     <a class="nav-link text-light" href=""><b>Profile</b></a>
+//                 </li>
+//                 <li class="nav-item">
+//                     <a class="nav-link text-light" href=""><b>Admin Deshboard</b></a>
+//                 </li>
+//                 <li class="nav-item">
+//                     <a class="nav-link text-light" href=""><b>Logout</b></a>
+//                 </li>
+//             `;
+//           } else {
+//             navElement.innerHTML += `
+//                 <li class="nav-item">
+//                     <a class="nav-link text-light" href=""><b>Home</b></a>
+//                 </li>
+//                 <li class="nav-item">
+//                     <a class="nav-link text-light" href=""><b>Profile</b></a>
+//                 </li>
+//                 <li class="nav-item">
+//                     <a class="nav-link text-light" href=""><b>Logout</b></a>
+//                 </li>
+//             `;
+//           }
+//         });
+//     } else {
+//       navElement.innerHTML += `
+//         <li class="nav-item">
+//             <a class="nav-link text-light" href=""><b>Home</b></a>
+//         </li>
+//         <li class="nav-item">
+//             <a class="nav-link text-light" href=""><b>Contact</b></a>
+//         </li>
+//         <li class="nav-item">
+//             <a class="nav-link text-light" href=""><b>About</b></a>
+//         </li>
+//         <li class="nav-item">
+//             <a class="nav-link text-light" href=""><b>Register</b></a>
+//         </li>
+//         <li class="nav-item">
+//             <a class="nav-link text-light" href=""><b>Login</b></a>
+//         </li>
+//       `;
+//     }
+//   });
+
+fetch("navbar.html")
+    .then((res) => res.text())
+    .then((data) => {
+        document.getElementById("navbar").innerHTML = data;
+
+        // Assign Auth Element
+        const navElement = document.getElementById("nav-element");
+
+        const token = localStorage.getItem("authToken");
+
+        if (token) {
+            navElement.innerHTML += `
+                <li class="nav-item">
+                    <a class="nav-link text-light" href=""><b>Home</b></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href=""><b>Profile</b></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href=""><b>Logout</b></a>
+                </li>
+            `;
+        }
+        else {
+            navElement.innerHTML += `
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="./index.html"><b>Home</b></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href=""><b>Contact</b></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href=""><b>About</b></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="./signup.html"><b>SignUp</b></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="./login.html"><b>Login</b></a>
+                </li>
+            `;
+        }
+    });
